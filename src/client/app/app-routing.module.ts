4d9cc9338ef6b1b70305437de78c0996883612c0
Soma Zsják <sanraith@users.noreply.github.com>
2021-08-08 20:03:45 +0200

    Update to Angular 12. Update packages.

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { CreateRoomComponent } from './create-room/create-room.component';
 import { HomeComponent } from './home/home.component';
 import { RoomComponent } from './room/room.component';
